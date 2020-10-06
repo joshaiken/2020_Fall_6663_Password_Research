5b211490e0a402bfa889a2523c9eb3196f6e8693
joshaiken <jdavidaiken@gmail.com>
2020-10-05 21:32:25 -0500

    Initial setup of Readme and test new environment

---
+++ PasswordGenerator.py
@@ -0,0 +1,14 @@
+#Author: Joshua Aiken
+#Title: Memorable Password Generator
+#Written: September 2020 to October 2020
+
+# About:
+# This tool was written as part of a research project for
+# 2020 Fall CSCI 6663 - Software Security
+# Professor Minhaz Zibran
+# to generate (hopefully, as thi sis the thesis being tested)
+# passwords that are both more memorable to the average
+# end-user than the average password, but also more computationally
+# complex than the average password. 
+
+print ("Hello World")
